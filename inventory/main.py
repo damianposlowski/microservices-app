da4cbc9757b2bb16f70d29a61c5b21ac60fdd16b
Damian <39247696+damianposlowski@users.noreply.github.com>
2023-02-09 13:11:49 +0100

    Setup redis and create product class

--- inventory/main.py
+++ inventory/main.py
@@ -2,9 +2,21 @@
 # uvicorn inventory.main:app --reload
 
 from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
+redis = get_redis_connection(
+    host = "redis-10769.c55.eu-central-1-1.ec2.cloud.redislabs.com",
+    port = 10769,
+    password = "####",
+    decode_responses = True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
 
 @app.get("/")
 async def root():
