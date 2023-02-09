c15e1b76ce3f729fb8f2a8323d9b4908498b0e1c
Damian <39247696+damianposlowski@users.noreply.github.com>
2023-02-09 12:43:25 +0100

    Initial commit

---
+++ inventory/main.py
@@ -0,0 +1,11 @@
+# Run FastAPI server with:
+# uvicorn main:app --reload
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
